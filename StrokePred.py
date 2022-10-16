a977a1d0c994ddab69fe8a9b0d39955fadee9576
ljacquesdedixmude <lucien.jacquesdedixmude@businessdecision.be>
2022-10-16 13:05:40 +0200

    Multi app to multi page

--- StrokePred.py
+++ StrokePred.py
@@ -133,19 +133,5 @@ cmap = sns.color_palette("crest", as_cmap=True)
 sns.heatmap(df.corr(),cmap = cmap, ax=ax, annot=True)
 st.write(fig)
 
-st.subheader(" Data Preparation")
-st.write("""
 
-* Handle Missing Values
-We need to handle the missing values in the BodyMassIndex column. 
-They are several options here we will fill the missing values with the mean value (28.89). 
-As they are few missing values (160 missing values for 4088 values total) this shouldn't
-mislead the models.
-
-* Create Dummy variables 
-For some models we will have to create dummy variables for the 
-categorical attributes ("Gender", "Married", "Employement_Type", "Urban_Rual", "Smoker"). When using
-PyCaret Library we don't need to as it handles categorical variables.
-
-""")
 
--- pages/1_dataprep.py
+++ pages/1_dataprep.py
@@ -2,16 +2,18 @@ import streamlit as st
 import matplotlib
 import pandas as pd
 
-def app(): 
-    st.subheader(" Data Preparation")
+st.subheader(" Data Preparation")
+st.write("""
 
-    st.write("""
+* Handle Missing Values
+We need to handle the missing values in the BodyMassIndex column. 
+They are several options here we will fill the missing values with the mean value (28.89). 
+As they are few missing values (160 missing values for 4088 values total) this shouldn't
+mislead the models.
 
-    We need to handle the missing values in the BodyMassIndex column. They 
-    are several options here we will fill the missing values with the mean value (28.89). 
-    As they are few missing values (160 missing values for 4088 values total) this shouldn't
-    mislead the models. For some models we will have to create dummy variables for the 
-    categorical attributes ("Gender", "Married", "Employement_Type", "Urban_Rual", "Smoker"). When using
-    PyCaret Library we don't need to as it handles categorical variables.
+* Create Dummy variables 
+For some models we will have to create dummy variables for the 
+categorical attributes ("Gender", "Married", "Employement_Type", "Urban_Rual", "Smoker"). When using
+PyCaret Library we don't need to as it handles categorical variables.
 
-    """)
+""")
